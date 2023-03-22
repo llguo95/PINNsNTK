e1abb3a8e314d40470881a50cc9fbc966e42293c
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2023-03-22 15:20:47 +0100

    Restructure project

---
+++ Poisson/main.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+from Compute_Jacobian import jacobian # Please download 'Compute_Jacobian.py' in the repository 
+import numpy as np
+import timeit
+from scipy.interpolate import griddata
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+from pinn import PINN
+from util import compute_ntk_eigenvalues
+from plot import *
+
+
+
+# Define solution and its Laplace
+a = 4
+
+def u(x, a):
+  return np.sin(np.pi * a * x)
+
+def u_xx(x, a):
+  return -(np.pi * a)**2 * np.sin(np.pi * a * x)
+
+
+for name, activation_function in zip(["tanh", "sigmoid", "softmax", "relu"], [tf.nn.tanh, tf.nn.sigmoid, tf.nn.softmax, tf.nn.relu]):
+    print("current used activation function:", name)
+    # Define computional domain
+    bc1_coords = np.array([[0.0],
+                        [0.0]])
+
+    bc2_coords = np.array([[1.0],
+                        [1.0]])
+
+    dom_coords = np.array([[0.0],
+                        [1.0]])
+
+    # Training data on u(x) -- Dirichlet boundary conditions
+
+    nn  = 100
+
+    X_bc1 = dom_coords[0, 0] * np.ones((nn // 2, 1))
+    X_bc2 = dom_coords[1, 0] * np.ones((nn // 2, 1))
+    X_u = np.vstack([X_bc1, X_bc2])
+    Y_u = u(X_u, a)
+
+    X_r = np.linspace(dom_coords[0, 0],
+                    dom_coords[1, 0], nn)[:, None]
+    Y_r = u_xx(X_r, a)
+
+
+
+
+    # Define model
+    layers = [1, 512, 1]  
+    # layers = [1, 512, 512, 512, 1]  
+    model = PINN(layers, X_u, Y_u, X_r, Y_r, activation_function=tf.nn.sigmoid)    
+
+
+
+    # Train model
+    # model.train(nIter=40001, batch_size=100, log_NTK=True, log_weights=True)
+    model.train(nIter=40001, batch_size=100, log_NTK=True, log_weights=True)
+
+
+
+
+    plot_loss(model.loss_bcs_log, model.loss_res_log)
+    plt.title(name)
+
+
+
+
+    nn = 1000
+    X_star = np.linspace(dom_coords[0, 0], dom_coords[1, 0], nn)[:, None]
+    u_star = u(X_star, a)
+    r_star = u_xx(X_star, a)
+
+    # Predictions
+    u_pred = model.predict_u(X_star)
+    r_pred = model.predict_r(X_star)
+    error_u = np.linalg.norm(u_star - u_pred, 2) / np.linalg.norm(u_star, 2)
+    error_r = np.linalg.norm(r_star - r_pred, 2) / np.linalg.norm(r_star, 2)
+
+    print('Relative L2 error_u: {:.2e}'.format(error_u))
+    print('Relative L2 error_r: {:.2e}'.format(error_r))
+
+
+
+    plot_resulting_func(X_star, u_star, u_pred)
+    plt.title(name)
+
+
+    # lambda_K_log, lambda_K_uu_log, lambda_K_rr_log, K_list = compute_ntk_eigenvalues(model)
+
+
+
+    # plot_eigenvalues(lambda_K_log, lambda_K_uu_log, lambda_K_rr_log)
+    # plt.title(name)
+
+
+    # plot_ntk_changes(K_list)
+    # plt.title(name)
+
+
+    # # Change of the weights and biases
+    # def compute_weights_diff(weights_1, weights_2):
+    #     weights = []
+    #     N = len(weights_1)
+    #     for k in range(N):
+    #         weight = weights_1[k] - weights_2[k]
+    #         weights.append(weight)
+    #     return weights
+
+    # def compute_weights_norm(weights, biases):
+    #     norm = 0
+    #     for w in weights:
+    #         norm = norm + np.sum(np.square(w))
+    #     for b in biases:
+    #         norm = norm + np.sum(np.square(b))
+    #     norm = np.sqrt(norm)
+    #     return norm
+
+    # # Restore the list weights and biases
+    # weights_log = model.weights_log
+    # biases_log = model.biases_log
+
+    # weights_0 = weights_log[0]
+    # biases_0 = biases_log[0]
+
+    # # Norm of the weights at initialization
+    # weights_init_norm = compute_weights_norm(weights_0, biases_0)
+
+    # weights_change_list = []
+
+    # N = len(weights_log)
+    # for k in range(N):
+    #     weights_diff = compute_weights_diff(weights_log[k], weights_log[0])
+    #     biases_diff = compute_weights_diff(biases_log[k], biases_log[0])
+        
+    #     weights_diff_norm = compute_weights_norm(weights_diff, biases_diff)
+    #     weights_change = weights_diff_norm / weights_init_norm
+    #     weights_change_list.append(weights_change)
+
+
+    # plot_weights_change(weights_change_list)
+    # plt.title(name)
+plt.show()
---
+++ Poisson/pinn.py
@@ -0,0 +1,247 @@
+import tensorflow as tf
+import numpy as np
+from Poisson.Compute_Jacobian import jacobian 
+import timeit
+
+class PINN:
+    def __init__(self, layers, X_u, Y_u, X_r, Y_r, activation_function=tf.nn.tanh):
+        self.mu_X, self.sigma_X = X_r.mean(0), X_r.std(0)
+        self.mu_x, self.sigma_x = self.mu_X[0], self.sigma_X[0]
+
+        # Normalize
+        self.X_u = (X_u - self.mu_X) / self.sigma_X
+        self.Y_u = Y_u
+        self.X_r = (X_r - self.mu_X) / self.sigma_X
+        self.Y_r = Y_r
+
+        # Safe activation_function
+        self.activation_func = activation_function
+
+        # Initialize network weights and biases
+        self.layers = layers
+        self.weights, self.biases = self.initialize_NN(layers)
+            
+        # Define the size of the Kernel
+        self.kernel_size = X_u.shape[0]
+        
+        # Define Tensorflow session
+        self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+        # Define placeholders and computational graph
+        self.x_u_tf = tf.placeholder(tf.float32, shape=(None, 1))
+        self.u_tf = tf.placeholder(tf.float32, shape=(None, 1))
+
+        self.x_bc_tf = tf.placeholder(tf.float32, shape=(None, 1))
+        self.u_bc_tf = tf.placeholder(tf.float32, shape=(None, 1))
+
+        self.x_r_tf = tf.placeholder(tf.float32, shape=(None, 1))
+        self.r_tf = tf.placeholder(tf.float32, shape=(None, 1))
+        
+        self.x_u_ntk_tf = tf.placeholder(tf.float32, shape=(self.kernel_size, 1))
+        self.x_r_ntk_tf = tf.placeholder(tf.float32, shape=(self.kernel_size, 1))
+
+
+        # Evaluate predictions
+        self.u_bc_pred = self.net_u(self.x_bc_tf)
+
+        self.u_pred = self.net_u(self.x_u_tf)
+        self.r_pred = self.net_r(self.x_r_tf)
+        
+        self.u_ntk_pred = self.net_u(self.x_u_ntk_tf)
+        self.r_ntk_pred = self.net_r(self.x_r_ntk_tf)
+     
+        # Boundary loss
+        self.loss_bcs = tf.reduce_mean(tf.square(self.u_bc_pred - self.u_bc_tf))
+
+        # Residual loss        
+        self.loss_res =  tf.reduce_mean(tf.square(self.r_tf - self.r_pred))
+        
+        # Total loss
+        self.loss = self.loss_res + self.loss_bcs
+
+        # Define optimizer with learning rate schedule
+        self.global_step = tf.Variable(0, trainable=False)
+        starter_learning_rate = 1e-5
+        self.learning_rate = tf.train.exponential_decay(starter_learning_rate, self.global_step,
+                                                        1000, 0.9, staircase=False)
+        # Passing global_step to minimize() will increment it at each step.
+        # To compute NTK, it is better to use SGD optimizer
+        # since the corresponding gradient flow is not exactly same.
+        self.train_op = tf.train.GradientDescentOptimizer(starter_learning_rate).minimize(self.loss)
+        # self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss, global_step=self.global_step)
+
+
+        # Initialize Tensorflow variables
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+        
+        self.saver = tf.train.Saver()
+        
+        # Compute the Jacobian for weights and biases in each hidden layer  
+        self.J_u = self.compute_jacobian(self.u_ntk_pred) 
+        self.J_r = self.compute_jacobian(self.r_ntk_pred)
+        
+        # The empirical NTK = J J^T, compute NTK of PINNs 
+        self.K_uu = self.compute_ntk(self.J_u, self.x_u_ntk_tf, self.J_u, self.x_u_ntk_tf)
+        self.K_ur = self.compute_ntk(self.J_u, self.x_u_ntk_tf, self.J_r, self.x_r_ntk_tf)
+        self.K_rr = self.compute_ntk(self.J_r, self.x_r_ntk_tf, self.J_r, self.x_r_ntk_tf)
+        
+        # Logger
+        # Loss logger
+        self.loss_bcs_log = []
+        self.loss_res_log = []
+
+        # NTK logger 
+        self.K_uu_log = []
+        self.K_rr_log = []
+        self.K_ur_log = []
+        
+        # Weights logger 
+        self.weights_log = []
+        self.biases_log = []
+        
+    # Xavier initialization
+    def xavier_init(self, size):
+        in_dim = size[0]
+        out_dim = size[1]
+        xavier_stddev = 1. / np.sqrt((in_dim + out_dim) / 2.)
+        return tf.Variable(tf.random.normal([in_dim, out_dim], dtype=tf.float32) * xavier_stddev,
+                           dtype=tf.float32)
+    
+    # NTK initialization
+    def NTK_init(self, size):
+        in_dim = size[0]
+        out_dim = size[1]
+        std = 1. / np.sqrt(in_dim)
+        return tf.Variable(tf.random.normal([in_dim, out_dim], dtype=tf.float32) * std,
+                           dtype=tf.float32)
+
+     # Initialize network weights and biases using Xavier initialization
+    def initialize_NN(self, layers):
+        weights = []
+        biases = []
+        num_layers = len(layers)
+        for l in range(0, num_layers - 1):
+            W = self.NTK_init(size=[layers[l], layers[l + 1]])
+            b = tf.Variable(tf.random.normal([1, layers[l + 1]], dtype=tf.float32), dtype=tf.float32)
+            weights.append(W)
+            biases.append(b)
+        return weights, biases
+
+    # Evaluates the forward pass
+    def forward_pass(self, H):
+        num_layers = len(self.layers)
+        for l in range(0, num_layers - 2):
+            W = self.weights[l]
+            b = self.biases[l]
+            H = self.activation_func(tf.add(tf.matmul(H, W), b))
+        W = self.weights[-1]
+        b = self.biases[-1]
+        H = tf.add(tf.matmul(H, W), b)
+        return H
+
+    # Evaluates the PDE solution
+    def net_u(self, x):
+        u = self.forward_pass(x)
+        return u
+
+    # Forward pass for the residual
+    def net_r(self, x):
+        u = self.net_u(x)
+
+        u_x = tf.gradients(u, x)[0] / self.sigma_x
+        u_xx = tf.gradients(u_x, x)[0] / self.sigma_x
+
+        res_u = u_xx
+        return res_u
+    
+    # Compute Jacobian for each weights and biases in each layer and retrun a list 
+    def compute_jacobian(self, f):
+        J_list =[]
+        L = len(self.weights)    
+        for i in range(L):
+            J_w = jacobian(f, self.weights[i])
+            J_list.append(J_w)
+     
+        for i in range(L):
+            J_b = jacobian(f, self.biases[i])
+            J_list.append(J_b)
+        return J_list
+    
+    # Compute the empirical NTK = J J^T
+    def compute_ntk(self, J1_list, x1, J2_list, x2):
+        D = x1.shape[0]
+        N = len(J1_list)
+        
+        Ker = tf.zeros((D,D))
+        for k in range(N):
+            J1 = tf.reshape(J1_list[k], shape=(D,-1))
+            J2 = tf.reshape(J2_list[k], shape=(D,-1))
+            
+            K = tf.matmul(J1, tf.transpose(J2))
+            Ker = Ker + K
+        return Ker
+            
+    # Trains the model by minimizing the MSE loss
+    def train(self, nIter=10000, batch_size=128, log_NTK=True, log_weights=True):
+
+        start_time = timeit.default_timer()
+        for it in range(nIter):
+            # Fetch boundary mini-batches
+            # Define a dictionary for associating placeholders with data
+            tf_dict = {self.x_bc_tf: self.X_u, self.u_bc_tf: self.Y_u,
+                       self.x_u_tf: self.X_u, self.x_r_tf: self.X_r,
+                       self.r_tf: self.Y_r
+                       }
+        
+            # Run the Tensorflow session to minimize the loss
+            self.sess.run(self.train_op, tf_dict)
+
+            # Print
+            if it % 100 == 0:
+                elapsed = timeit.default_timer() - start_time
+                loss_value = self.sess.run(self.loss, tf_dict)
+                loss_bcs_value, loss_res_value = self.sess.run([self.loss_bcs, self.loss_res], tf_dict)
+                self.loss_bcs_log.append(loss_bcs_value)
+                self.loss_res_log.append(loss_res_value)
+
+                print('It: %d, Loss: %.3e, Loss_bcs: %.3e, Loss_res: %.3e ,Time: %.2f' %
+                      (it, loss_value, loss_bcs_value, loss_res_value, elapsed))
+                
+
+                start_time = timeit.default_timer()
+
+            if log_NTK:
+                # provide x, x' for NTK
+                if it % 100 == 0:
+                    print("Compute NTK...")
+                    tf_dict = {self.x_u_ntk_tf: self.X_u, self.x_r_ntk_tf: self.X_r}
+                    K_uu_value, K_ur_value, K_rr_value = self.sess.run([self.K_uu,
+                                                                        self.K_ur,
+                                                                        self.K_rr], tf_dict)
+                    self.K_uu_log.append(K_uu_value)
+                    self.K_ur_log.append(K_ur_value)
+                    self.K_rr_log.append(K_rr_value)
+                    
+            if log_weights:
+                if it % 100 ==0:
+                    print("Weights stored...")
+                    weights = self.sess.run(self.weights)
+                    biases = self.sess.run(self.biases)
+                    
+                    self.weights_log.append(weights)
+                    self.biases_log.append(biases)
+                
+    # Evaluates predictions at test points
+    def predict_u(self, X_star):
+        X_star = (X_star - self.mu_X) / self.sigma_X
+        tf_dict = {self.x_u_tf: X_star}
+        u_star = self.sess.run(self.u_pred, tf_dict)
+        return u_star
+
+    # Evaluates predictions at test points
+    def predict_r(self, X_star):
+        X_star = (X_star - self.mu_X) / self.sigma_X
+        tf_dict = {self.x_r_tf: X_star}
+        r_star = self.sess.run(self.r_pred, tf_dict)
+        return r_star
